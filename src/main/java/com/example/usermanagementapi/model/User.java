ff78e31d6efbc19720dbc53d3e1b4a8f46aa50eb
leelaparitala2004 <leelanandanparitala33@gmail.com>
2025-07-08 09:28:33 +0530

    added java springboot security

--- src/main/java/com/example/usermanagementapi/model/User.java
+++ src/main/java/com/example/usermanagementapi/model/User.java
@@ -1,42 +1,71 @@
 package com.example.usermanagementapi.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
 
 @Entity
-@Table (name = "users")
+@Table(name="users")
+@Data
+@NoArgsConstructor//default constructor
+@RequiredArgsConstructor
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NonNull
+    @NotBlank(message = "Name is required")
+    @Size(min = 2,max = 100,message = "Name must be between 2 and 100 characters")
     private String name;
+
+    @NonNull
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    @Column(unique = true)
     private String email;
 
-    // Constructors
-    public User() {}
+    //@Valid
+    @NotBlank(message = "Password is required")
+    private String password;
 
-    public User(String name, String email) {
-        this.name = name;
-        this.email = email;
-    }
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles",joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
+    private Set<String> roles = new HashSet<>();
 
-    // Getters & Setters
-    public Long getId() {
-        return id;
-    }
+    @Column(name="created_at",updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
-    public void setId(Long id) { this.id = id; }
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) { this.name = name; }
 
     public String getEmail() {
-        return email;
+        return "";
     }
 
-    public void setEmail(String email) { this.email = email; }
+    public String getPassword() {
+        return "";
+    }
+
+    public Collection<Object> getRoles() {
+        return java.util.List.of();
+    }
 }
